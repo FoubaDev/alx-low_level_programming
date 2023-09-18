db73b442137d43b90d24f57786a5eddcce8b30f9
foubadev <foubabertrand@gmail.com>
2023-09-18 21:46:42 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: string to check
+ */
+
+void puts2(char *str)
+{
+
+	int length = 0;
+
+	while (*(str + length) != '\0')
+	{
+		if (length % 2 == 0)
+			_putchar(*(str + length));
+		length++;
+	}
+
+	_putchar('\n');
+}
