6448e1ce7331249ce30bd3889b18f28d1a526c7a
foubadev <foubabertrand@gmail.com>
2023-10-30 14:01:02 +0100

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,8 +4,8 @@
  * read_textfile - reads a text file and prints it to std output
  * @filename: file to read and print
  * @letters: number of letters it should read and print
- *
- * Return: actual number of letters read and printed
+ 
+ * Return: actual number of letter read and printed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
