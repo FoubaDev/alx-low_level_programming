03bae8059172a98c406c5609da6d8cced35b2178
foubadev <foubabertrand@gmail.com>
2023-10-31 08:43:02 +0100

    Read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Function that reads a text file and prints it to the POSIX
+ * read_textfile - Function that reads a text file and return it to the POSIX
  * standard output
  *
  * @filename: Name of the file to be red
